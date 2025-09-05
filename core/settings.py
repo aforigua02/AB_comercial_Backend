873aa572fa24d42dae561ba2e044236007e0b84f
aforigua02 <pipeforigua@gmail.com>
2025-09-05 18:46:19 -0500

    Ajustes de la ruta db

--- core/settings.py
+++ core/settings.py
@@ -99,11 +99,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+    )
 }
+
 
 
 # Password validation
